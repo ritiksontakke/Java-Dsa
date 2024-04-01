356760b7e7adbdd4d4dd23f91dff0921e219caec
Ritik Arun Sontakke <ritiksontakke1008@gmail.com>
2024-04-01 17:18:39 +0530

    add sum in question dsa java

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,6 +1,13 @@
-public class javaBasics{
+import java.util.*;
+
+
+public class JavaBasics{
    public static void main(String args[]){
-    System.out.print("Hello ritik");
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = a + b;
+    System.out.println(sum);
       
 
    }
