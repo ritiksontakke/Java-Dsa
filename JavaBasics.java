ae2b66d47722a4312c755d1cd47baf954ef839f7
Ritik Arun Sontakke <ritiksontakke1008@gmail.com>
2024-04-30 11:14:14 +0530

    dsa

--- JavaBasics.java
+++ JavaBasics.java
@@ -2,11 +2,8 @@ import java.util.*;
 
 public class JavaBasics{
    public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
-    int b = sc.nextInt();
-    int sum = a * b;
-    System.out.println(sum);
-    
+  Scanner sc = new Scanner (System.in);
+  String name = sc.nextLine();
+  System.out.print(name);
    }
 }
