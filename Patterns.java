15207923ffff2b8efecd777b484508a8dcb26d91
Ritik Arun Sontakke <ritiksontakke1008@gmail.com>
2024-05-02 18:08:47 +0530

    .

--- Patterns.java
+++ Patterns.java
@@ -15,7 +15,25 @@ public class Patterns{
     }
    }
 
+
+   public static void half_pre(int n){
+     for(int i=1 ; i<=n; i++){
+        for(int j=1 ; j<=n-i; j++){
+            System.out.print(" ");
+        }
+
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+     }
+   }
+
+
+
+
    public static void main(String args[]){
-      hello_rectangles(4,5);
+    //   hello_rectangles(4,5);
+       half_pre(4);
    }
 }
