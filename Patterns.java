e138e27392ef04a36360b04972dae55bd30d0748
Ritik Arun Sontakke <ritiksontakke1008@gmail.com>
2024-05-03 11:15:42 +0530

    floyad trangels dsa que

--- Patterns.java
+++ Patterns.java
@@ -30,10 +30,36 @@ public class Patterns{
    }
 
 
+    public static void Pra_Numbers(int n){
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i+1; j++) {
+                System.out.print(j);
+            }
 
+            System.out.println();
+        }
+    }
+       
+     public static void Floyd_trangle(int n){
+         
+         int counter = 1;
+         for(int i=1; i<=n; i++){
+          for(int j=1; j<=i; j++)
+          {
+              System.out.print(counter + " ");
+              counter++;
+          }
+          System.out.println();
+         }
+
+
+     }
+     
 
    public static void main(String args[]){
     //   hello_rectangles(4,5);
-       half_pre(4);
+    //    half_pre(4);
+    // Pra_Numbers(5);
+    Floyd_trangle(4);
    }
 }
